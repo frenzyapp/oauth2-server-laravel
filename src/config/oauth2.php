576ae203a20c3382b6ed7b82ddb30ca637180c94
Martin Bastien <martin.bastien@studiofrenetic.com>
2015-01-29 12:05:30 -0500

    add default grant types
    Signed-off-by: Martin Bastien <martin.bastien@studiofrenetic.com>

--- src/config/oauth2.php
+++ src/config/oauth2.php
@@ -75,7 +75,48 @@ return [
     */
 
     'grant_types' => [
+        'authorization_code' => [
+            'class'            => 'League\OAuth2\Server\Grant\AuthCodeGrant',
+            'access_token_ttl' => 3600,
 
+            // the authorization code time to live
+            'auth_token_ttl'   => 3600,
+        ],
+        
+        'password' => [
+            'class'            => 'League\OAuth2\Server\Grant\PasswordGrant',
+            'access_token_ttl' => 604800,
+
+            // the code to run in order to verify the user's identity
+            'callback'         => function($username, $password){
+                $credentials = [
+                    'email'    => $username,
+                    'password' => $password,
+                ];
+
+                if (Auth::once($credentials)) {
+                    return Auth::user()->id;
+                } else {
+                    return false;
+                }
+            }
+        ],
+
+        'client_credentials' => [
+            'class'                 => 'League\OAuth2\Server\Grant\ClientCredentialsGrant',
+            'access_token_ttl'      => 3600,
+        ],
+
+        'refresh_token' => [
+            'class'                 => 'League\OAuth2\Server\Grant\RefreshTokenGrant',
+            'access_token_ttl'      => 3600,
+
+            // the refresh token time to live
+            'refresh_token_ttl'     => 604800,
+
+            // whether or not to issue a new refresh token when a new access token is issued
+            'rotate_refresh_tokens' => false,
+        ],
     ],
 
     /*
